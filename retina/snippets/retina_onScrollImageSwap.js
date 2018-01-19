628dd7713590ac20c9e4241dc621608866cd9300
russell2shea <russell2shea@gmail.com>
2018-01-19 15:08:11 -0500

    Add files via upload

---
+++ retina/snippets/retina_onScrollImageSwap.js
@@ -0,0 +1,66 @@
+jQuery( document ).ready(function($) {
+
+		$( '#gif1' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/Dcred_Compariosn_Chart_Example.gif');
+		});
+		$( '#gif2' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos_1/AF/CSK/STRAT_Compariosn_Chart_Example.gif');
+		});
+		$( '#gif3' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos_1/AF/CSK/SHIFT_Compariosn_Chart_Example.gif');
+		});
+		$( '#gif4' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/MONOCOIN_Compariosn_Chart_Example.gif');
+		});
+		$( '#gif5' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/BITCOINC_Compariosn_Chart_Example.gif');
+		});
+		$( '#gif6' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/MAID_Compariosn_Chart_Example.gif');
+		});
+		$( '#gif7' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/DRC_Compariosn_Chart_Example.gif');
+		});
+		$( '#gif8' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Example_First_001.gif');
+		});
+		$( '#gif9' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_Decred_001.gif');
+		});
+		$( '#gif10' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_STRAT_001.gif');
+		});
+		$( '#gif11' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_SHIFT_001.gif');
+		});
+		$( '#gif12' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_RIPPLE_001.gif');
+		});
+		$( '#gif13' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_MONACOIN_001.gif');
+		});
+		$( '#gif14' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_BTS_001.gif');
+		});
+		$( '#gif15' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_BCH_001.gif');
+		});
+		$( '#gif16' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_MAID_001.gif');
+		});
+		$( '#gif17' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_SIGNAL_001.gif');
+		});
+		$( '#gif18' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_0118_Animated_DRC_001.gif');
+		});
+		$( '#gif19' ).bind( 'scrollin', { distance: 100 }, function() {
+			$(this).attr("src", 'https://d13p2xj50zkyqm.cloudfront.net/promos/AF/CSK/CSK_8MillionCrypto_Image_Code_Scrolling.gif');
+		});
+
+
+
+
+});
+
+
